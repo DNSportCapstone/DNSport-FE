9d5362bbaed2f3d23fd5693d447e9517ecfdad0a
SecondHand1 <choyvinhthien0209@gmail.com>
2025-02-28 23:34:23 +0700

    DNS - Implement common function

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -1,6 +1,19 @@
-export function formatVND(number) {
-  return new Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND",
-  }).format(number);
-}
+import store from "../../store";
+import { jwtDecode } from "jwt-decode";
+
+const CommonHelper = {
+  formatVND(number) {
+    return new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(number);
+  },
+  getCurrentUserId() {
+    const accessTokenDecoded = jwtDecode(
+      store.getters.accessToken ?? localStorage.getItem("accessToken")
+    );
+    return accessTokenDecoded.userId;
+  },
+};
+
+export default CommonHelper;
