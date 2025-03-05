530a07dfe02acbfc66d31a536f5a57f0e08f76f9
Bnguyen <ngobaonguyen050803@gmail.com>
2025-03-05 21:03:39 +0700

    Rating UI, Update login Page UI

---
+++ src/components/CommentSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="comment-section">
+    <textarea
+      v-model="newComment"
+      placeholder="Write a comment..."
+      @input="updateComment"
+    />
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">
+        <strong>{{ comment.user }}</strong
+        >: {{ comment.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comments: Array,
+  },
+  data() {
+    return {
+      newComment: "",
+    };
+  },
+  methods: {
+    updateComment() {
+      this.$emit("update-comment", this.newComment);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.comment-section {
+  max-width: 600px;
+  margin-top: 20px;
+}
+
+textarea {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
